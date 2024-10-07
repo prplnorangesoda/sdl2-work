055b5deda95e52df6a76345833b4d3989354da55
Lucy Faria <rafploooz@gmail.com>
2024-10-07 15:27:46 -0400

    Further text (minecraft debug)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use simple_logger::SimpleLogger;
 use std::sync::Arc;
 use std::sync::RwLock;
 use std::thread;
@@ -25,7 +26,7 @@ impl Default for AppState {
 }
 
 fn update(state: &AppState) -> AppState {
-    println!("Current iteration: {0}", state.current_iter);
+    log::trace!("Current update iteration: {0}", state.current_iter);
     AppState {
         should_shutdown: false,
         current_iter: (state.current_iter + 1) % u64::MAX,
@@ -33,6 +34,12 @@ fn update(state: &AppState) -> AppState {
 }
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .env()
+        .init()
+        .expect("Logger should create");
+    log::info!("Logger initialized");
     let sdl_context = sdl2::init().unwrap();
 
     let video_subsystem = sdl_context.video().unwrap();
@@ -65,7 +72,7 @@ fn main() {
     let game_thread = thread::spawn(move || {
         let state_rwlock = extra_state;
         'thread_inner: loop {
-            thread::sleep(Duration::from_millis(100));
+            thread::sleep(Duration::from_nanos(16_666_667));
             let state = {
                 let state = state_rwlock.read().unwrap();
                 if state.should_shutdown {
@@ -95,8 +102,8 @@ fn main() {
         let delta_time =
             ((now - last) * 1000) as f64 / timer_subsystem.performance_frequency() as f64;
 
-        println!("delta_time: {delta_time}");
-        println!("loose_i: {loose_i}");
+        log::debug!("delta_time: {delta_time}");
+        log::debug!("loose_i: {loose_i}");
 
         loose_i = loose_i + (delta_time / 10.0);
         let i = (loose_i % 255.0).floor() as u8;
@@ -105,19 +112,20 @@ fn main() {
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { timestamp } => {
-                    println!("Received message to quit at timestamp: {timestamp}");
+                Event::Quit { timestamp }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    timestamp,
+                    ..
+                } => {
+                    log::info!("Received message to quit at timestamp: {timestamp}, shutting down gracefully, waiting for write lock");
                     let mut state = state.write().unwrap();
-                    println!("Write lock acquired");
+                    log::debug!("Write lock acquired");
                     state.should_shutdown = true;
                     drop(state);
                     game_thread.join().unwrap();
                     break 'running;
                 }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
                 _ => {}
             }
         }
@@ -128,7 +136,7 @@ fn main() {
                 "Current game thread iteration: {0}",
                 state.current_iter
             ))
-            .shaded(Color::RGB(255, 255, 255), Color::RGB(0, 0, 0))
+            .shaded(Color::RGB(255, 255, 255), Color::RGBA(0, 0, 0, 50))
             .unwrap();
 
         let font_surf2 = caskaydia_font
@@ -136,12 +144,18 @@ fn main() {
                 "Current render thread iteration: {0}",
                 frame_count
             ))
-            .shaded(Color::RGB(255, 255, 255), Color::RGB(0, 0, 0))
+            .shaded(Color::RGB(255, 255, 255), Color::RGBA(0, 0, 0, 50))
+            .unwrap();
+        let font_surf3 = caskaydia_font
+            .render(&format!("delta_time: {0}", delta_time))
+            .shaded(Color::RGB(255, 255, 255), Color::RGBA(0, 0, 0, 50))
             .unwrap();
         let (wide1, tall1) = (font_surf.width(), font_surf.height());
         let (wide2, tall2) = (font_surf2.width(), font_surf2.height());
-        println!("Wide: {wide1}, tall: {tall1}");
-        println!("Wide2: {wide2}, tall2: {tall2}");
+        let (wide3, tall3) = (font_surf3.width(), font_surf3.height());
+        log::trace!("Wide1: {wide1}, tall1: {tall1}");
+        log::trace!("Wide2: {wide2}, tall2: {tall2}");
+        log::trace!("Wide3: {wide3}, tall3: {tall3}");
 
         let tex = texture_creator
             .create_texture_from_surface(font_surf)
@@ -149,14 +163,19 @@ fn main() {
         let tex2 = texture_creator
             .create_texture_from_surface(font_surf2)
             .unwrap();
+        let tex3 = texture_creator
+            .create_texture_from_surface(font_surf3)
+            .unwrap();
 
         let texture_rect = Rect::new(50, 50, wide1, tall1);
-        let texture_rect2 = Rect::new(50, 50 + (i32::try_from(tall1).unwrap()), wide2, tall2);
+        let texture_rect2 = Rect::new(50, 50 + i32::try_from(tall1).unwrap(), wide2, tall2);
+        let texture_rect3 = Rect::new(50, 50 + i32::try_from(tall1 + tall2).unwrap(), wide3, tall3);
 
         canvas.copy(&tex, None, texture_rect).unwrap();
         canvas.copy(&tex2, None, texture_rect2).unwrap();
+        canvas.copy(&tex3, None, texture_rect3).unwrap();
 
         canvas.present();
     }
-    println!("Bye, world!");
+    log::info!("Bye, world!");
 }
